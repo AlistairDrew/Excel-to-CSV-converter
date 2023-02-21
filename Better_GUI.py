b994cff83923393c00a51ee32e672a138cb394f7
Alistair Drew <alistairdrew37@gmail.com>
2023-02-21 08:16:57 +0000

    extra tests for GUI and functionality

--- Better_GUI.py
+++ Better_GUI.py
@@ -2,38 +2,41 @@ import openpyxl
 import tkinter as tk
 from tkinter import filedialog
 
-# Define a Tkinter window for the file dialog
+def get_column_letter(column_index):
+    """Get the column letter from the column index"""
+    dividend = column_index
+    column_name = ''
+    while dividend > 0:
+        modulo = (dividend - 1) % 26
+        column_name = chr(65 + modulo) + column_name
+        dividend = (dividend - modulo) // 26
+    return column_name
+
 root = tk.Tk()
 root.withdraw()
 
-# Ask the user to select an excel file
 file_path = filedialog.askopenfilename()
 
-# Load the selected excel file
 workbook = openpyxl.load_workbook(file_path)
 worksheet = workbook.active
 
-# Define a Tkinter window for managing the column headings
 headings_window = tk.Toplevel()
 headings_window.title('Manage Column Headings')
 
-# Create a label for the current column headings
 current_headings_label = tk.Label(headings_window, text='Current Column Headings:')
 current_headings_label.grid(row=0, column=0)
 
-# Create a listbox for displaying the current column headings
 current_headings_listbox = tk.Listbox(headings_window)
 for col in worksheet.iter_cols(1, worksheet.max_column):
     current_headings_listbox.insert(tk.END, col[0].value)
 current_headings_listbox.grid(row=1, column=0)
 
-# Create a label and entry for adding a new column heading
 new_heading_label = tk.Label(headings_window, text='Add New Column Heading:')
 new_heading_label.grid(row=2, column=0)
+
 new_heading_entry = tk.Entry(headings_window)
 new_heading_entry.grid(row=3, column=0)
 
-# Create a button for adding a new column heading
 def add_heading():
     new_heading = new_heading_entry.get()
     if new_heading:
@@ -45,7 +48,6 @@ def add_heading():
 add_heading_button = tk.Button(headings_window, text='Add', command=add_heading)
 add_heading_button.grid(row=3, column=1)
 
-# Create a button for importing column headings
 def import_headings():
     import_file_path = filedialog.askopenfilename()
     import_workbook = openpyxl.load_workbook(import_file_path)
@@ -57,49 +59,13 @@ def import_headings():
 import_button = tk.Button(headings_window, text='Import Headings', command=import_headings)
 import_button.grid(row=4, column=0)
 
-# Create a button for clearing the column headings
 def clear_headings():
     for col in worksheet.iter_cols(1, worksheet.max_column):
         col[0].value = None
     current_headings_listbox.delete(0, tk.END)
 
 clear_button = tk.Button(headings_window, text='Clear', command=clear_headings)
-clear_button.grid(row=4, column=1)
+clear_button.grid(row=4,
 
-# Create a button for saving the changes and closing the window
-def save_and_close():
-    workbook.save(file_path)
-    headings_window.destroy()
-
-save_button = tk.Button(headings_window, text='Save', command=save_and_close)
-save_button.grid(row=5, column=0)
-
-# Create a label and entry for mapping the data to the column headings
-map_data_label = tk.Label(headings_window, text='Map Data:')
-map_data_label.grid(row=6, column=0)
-map_data_entry = tk.Entry(headings_window)
-map_data_entry.grid(row=7, column=0)
-
-# Create a button for mapping the data to the column headings
-#def map_data():
-#    for row in worksheet.iter_rows(2, worksheet.max_row):
-#        data_dict = {current_headings_listbox.get(i): cell.value for i, cell in enumerate(row)}
-#        print(data_dict)  # Replace with desired behavior
-
-#map_data_button = tk.Button(headings_window, text='Map Data', command=map_data)
-#map_data_button.grid(row=5, column=0)
-
- # Create a button for saving the changes and closing the window
-def save_and_close():
-    workbook.save(file_path)
-    headings_window.destroy()
-
-save_button = tk.Button(headings_window, text='Save',command=save_and_close)
-save_button.grid(row=5, column=0)
-
-# Run the Tkinter event loop
-headings_window.mainloop()
-
-print('Column headings saved successfully!')
 
 
